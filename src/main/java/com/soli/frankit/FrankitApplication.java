cf5585a84a6c7f0d3ae324a3f064db83dbe90d87
print(soli) <eumsoli@gmail.com>
2025-02-17 16:49:41 +0900

    🎉 begin frankit project!

---
+++ src/main/java/com/soli/frankit/FrankitApplication.java
@@ -0,0 +1,13 @@
+package com.soli.frankit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FrankitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FrankitApplication.class, args);
+    }
+
+}
