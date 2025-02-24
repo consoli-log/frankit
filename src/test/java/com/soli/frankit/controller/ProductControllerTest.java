775f957cb45cae2cda917dd0e6111c13b31734f1
consoli.log <eumsoli@gmail.com>
2025-02-24 16:36:01 +0900

    🐛 Swagger 호환성 문제로 Spring Boot 버전 다운그레이드 1. Swagger와의 호환성을 위해 Spring Boot 버전 조정 2. 테스트 @MokitoBean -> @MockBean으로 변경

--- src/test/java/com/soli/frankit/controller/ProductControllerTest.java
+++ src/test/java/com/soli/frankit/controller/ProductControllerTest.java
@@ -14,12 +14,12 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.context.annotation.Import;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.http.MediaType;
-import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.test.web.servlet.MockMvc;
 
 import java.math.BigDecimal;
@@ -45,10 +45,10 @@ class ProductControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockitoBean
+    @MockBean
     private ProductService productService;
 
-    @MockitoBean
+    @MockBean
     private OrderService orderService;
 
     private Long validId;
